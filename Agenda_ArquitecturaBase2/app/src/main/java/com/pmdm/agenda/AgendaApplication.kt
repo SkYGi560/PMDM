fa60359ab93959a245321afd4f3caa113ef94561
Joel Vives <joelvivesvidal@gmail.com>
2025-10-13 12:34:35 +0200

    noseee

--- Agenda_ArquitecturaBase2/app/src/main/java/com/pmdm/agenda/AgendaApplication.kt
+++ Agenda_ArquitecturaBase2/app/src/main/java/com/pmdm/agenda/AgendaApplication.kt
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 package com.pmdm.agenda
 
 import android.app.Application
 
+=======
+package com.pmdm.agenda
+
+import android.app.Application
+
+>>>>>>> b89b961 (Ejercicios basicos aplicaciones2)
 class AgendaApplication : Application()
